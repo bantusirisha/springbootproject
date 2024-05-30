671ce2f65581bbe91896505ad88f8ee5e1f7e0ee
bantusirisha <150772036+bantusirisha@users.noreply.github.com>
2024-05-30 15:52:11 +0530

    Delete demospring directory

--- demospring/javaEx/ProdRepo.java
+++
@@ -1,9 +0,0 @@
-package siri.employeeProject.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import siri.employeeProject.entity.Prod;
-
-public interface ProdRepo extends JpaRepository<Prod, Integer> {
-
-//    List<Prod> findByCat(Cat cat);
-}
